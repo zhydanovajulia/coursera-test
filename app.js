8e6792c669c3334514ceaabfd9932ceb0aa62b4e
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2016-09-17 22:14:47 +0300

    fix

--- app.js
+++ app.js
@@ -9,22 +9,19 @@
   function LunchCheckController($scope) {
     $scope.menu = ""
     $scope.message = ""
-    console.log($scope.message)
-      $scope.check = function() {
-        if ($scope.menu.length > 0) {
-          $scope.message = checkString($scope.menu)
+    // console.log($scope.message)
+    $scope.check = function() {
+      if ($scope.menu.length > 0) {
+        // console.log($scope.menu)
+        var amount = $scope.menu.split(',').filter(function(n){ return n != " " && n != "" }).length
+        if (amount > 3 ) {
+         $scope.message = "Too much!"
+        } else if (amount < 3 && amount > 0) {
+          $scope.message = "Enjoy!"
         } else {
           $scope.message = "Please enter data first"
         }
       }
-  }
-
-  function checkString(menu) {
-    var amount = menu.split(',').filter(function(n){ return n != " " && n != "" }).length
-    if (amount > 3) {
-      return "Too much!"
-    } else {
-      return "Enjoy!"
     }
   }
 
