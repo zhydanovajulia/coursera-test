642ecb26567bb5dfbb5f2b20a1bf4db9507de65f
Iuliia Tsaplina <zhydanovajulia@gmail.com>
2016-09-17 22:29:35 +0300

    add colors

--- app.js
+++ app.js
@@ -15,12 +15,22 @@
         // console.log($scope.menu)
         var amount = $scope.menu.split(',').filter(function(n){ return n != " " && n != "" }).length
         if (amount > 3 ) {
-         $scope.message = "Too much!"
+          $scope.message = "Too much!"
+          $scope.statusInput = "has-success"
+          $scope.statusText = "text-success"
         } else if (amount <= 3 && amount > 0) {
           $scope.message = "Enjoy!"
+          $scope.statusInput = "has-success"
+          $scope.statusText = "text-success"
         } else {
           $scope.message = "Please enter data first"
+          $scope.statusText = "text-danger"
+          $scope.statusInput = "has-error"
         }
+      } else {
+        $scope.message = "Please enter data first"
+        $scope.statusText = "text-danger"
+        $scope.statusInput = "has-error"
       }
     }
   }
